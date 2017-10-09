75a2fe743ab18757c69e32c14562fbb877d38885
Albert Khasanov <albert@gestiv.ru>
2017-10-09 09:34:41 +0600

    Fix bug

--- Lesson2/array_numbers.rb
+++ Lesson2/array_numbers.rb
@@ -1,5 +1,9 @@
 array_numbers = (10..100).map do |i|
-  i if (i % 5).zero?
-end
+  i if (i % 5).zero? && (i % 5) != nil
+end.compact
+
+# 2 й способ
+#array_numbers = (10..100).select {|i| i if (i % 5).zero?}.map { |i| i }
+
 puts array_numbers.inspect
 
--- Lesson2/month_hash.rb
+++ Lesson2/month_hash.rb
@@ -3,8 +3,6 @@ month_hash = { january: 31, february: 28, mach: 31, april: 30, may: 31,
                june: 30, jule: 31, august: 31, september: 30,
                october: 31, november: 30, december: 31 }
 
-month_hash.each do |month, count_days|
-  if count_days == 30
-    puts "#{month} - #{count_days}"
-  end
+month_hash.each do |month, days|
+  puts "#{month} - #{days}" if days == 30
 end
