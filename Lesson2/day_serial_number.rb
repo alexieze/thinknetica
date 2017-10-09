be28da7f75b1d5a232f8df43635ff84fe9f27b8d
Albert Khasanov <albert@gestiv.ru>
2017-10-09 20:51:08 +0600

    Fix bug

--- Lesson2/day_serial_number.rb
+++ Lesson2/day_serial_number.rb
@@ -7,7 +7,7 @@ year = gets.chomp.to_i
 
 months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-leap_year = ((year%4 == 0 and year%100 != 0) or (year%400 == 0)) ? true : false
+leap_year = ((year%4 == 0 && year%100 != 0) || (year%400 == 0)) ? true : false
 months[1] += 1 if leap_year
 
 day_serial_number = day
