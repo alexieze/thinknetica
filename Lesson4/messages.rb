9b881da018f385c250d4f05f1aa8f53171bd9192
Albert Khasanov <albert@gestiv.ru>
2017-10-22 19:52:33 +0600

    RailStation v0.0.1

--- Lesson4/messages.rb
+++ Lesson4/messages.rb
@@ -2,9 +2,12 @@ class Messages
 
   def commands
     puts 'Выберите функцию управления'
-    puts 'manage_stations - Показать список станций'
-    puts 'manage_trains - Добавить станцию'
-    puts 'manage_carriages - Управлять вагонами'
-    puts 'manage_routes - Управлять маршрутами'
+    puts 'add_stations - Показать список станций'
+    puts 'add_train - Показать список станций'
+    puts 'add_carriage - Показать список станций'
+    puts 'list_stations - Показать список станций'
+    puts 'add_route'
+    puts 'list_trains'
+    puts ''
   end
 end
--- Lesson4/rail_station_company.rb
+++ Lesson4/rail_station_company.rb
@@ -11,22 +11,91 @@ class RailStationCompany
 
 
   def run()
-    @message.commands
     run_process
   end
 
   private
 
   def run_process
-    puts 'Введите команду:'
-    task = gets.chomp.to_s.downcase
     loop do
-      if task == 'manage_stations'
+      @message.commands
+      puts 'Введите команду:'
+      task = gets.chomp.to_s.downcase
+      if task == 'add_station'
+        puts 'Введите название станции:'
+        station_name = gets.chomp.to_s
+        if !check_station?(station_name)
+          station = Station.new(gets.chomp.to_s)
+          @stations << station
+        else
+          puts 'Станция с таким именем уже существует'
+        end
+      elsif task == 'list_stations'
+        puts 'Список станций:'
+        list_stations
+      elsif task == 'add_train'
+        puts 'Какой поезд нужно добавить'
+        puts '1 - Пасажирский'
+        puts '2 - Грузовой'
+        train_type = gets.chomp.to_i
+        if train_type == 1
+          puts 'Введите номер поезда'
+          train_number = gets.chomp.to_i
+          puts 'Введите имя поезда'
+          train_name = gets.chomp.to_s
+          train = PassengerTrain.new(train_number, train_name)
+        elsif train_type == 2
+          puts 'Введите номер поезда'
+          train_number = gets.chomp.to_i
+          puts 'Введите имя поезда'
+          train_name = gets.chomp.to_s
+          train = PassengerTrain.new(train_number, train_name)
+        end
+        @trains << train
+      elsif task == 'list_trains'
+        list_trains
+      elsif task == 'add_route'
 
+      elsif task == 'list_route'
+      elsif task == 'exit'
+        break
       end
-      break
+      next
     end
   end
 
-  def manage ()
+  private
+
+  def list_stations
+    @stations.each do |station|
+      puts station.name
+    end
+  end
+
+  def list_trains
+    @trains.each do |train|
+      puts train.name
+    end
+  end
+
+  def get_train_by_name(name, type_train)
+    @trains.select do |train|
+      train if train.name.downcase == name.downcase && train.kind_of?(type_train)
+    end
+  end
+
+  def get_station_by_name(name)
+    @stations.select do |station|
+      station if station.name.downcase == name.downcase
+    end
+  end
+
+  def check_station?(name)
+    @stations.each do |station|
+      return true if station.name.downcase == name.downcase
+    end
+    false
+  end
+
+
 end
